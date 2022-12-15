8c7468f7409ea50fbb4473cc173459ce39e11eee
Nastasiia03 <abernatskao@gmail.com>
2022-12-15 23:09:00 +0200

    task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,29 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// 1
+const gallery = document.querySelector(".gallery");
+const markup = galleryItems.map(({preview, original, description}) => `<div class="gallery__item"><a class="gallery__link" href="${original}"><img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/></a></div>`)
+gallery.insertAdjacentHTML('beforeend', markup); 
+
+
+function onClick(event) {
+    event.preventDefault();
+    if (event.target.nodeName !== "IMG") {
+        return
+    };
+
+    const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" width="800" height="600">
+`)
+    instance.show()
+
+    gallery.addEventListener("keydown", (event) => {
+        if (event.code === "Escape") {
+            instance.close();
+        }
+    })
+} 
+
+gallery.addEventListener("click", onClick)
+
