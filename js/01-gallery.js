b24b2df2a33cd2bd762373f0d7e45bca8037e7e2
Nastasiia03 <abernatskao@gmail.com>
2022-12-17 13:07:54 +0200

    task 2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -3,7 +3,7 @@ import { galleryItems } from './gallery-items.js';
 
 // 1
 const gallery = document.querySelector(".gallery");
-const markup = galleryItems.map(({preview, original, description}) => `<div class="gallery__item"><a class="gallery__link" href="${original}"><img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/></a></div>`)
+const markup = galleryItems.map(({ preview, original, description }) => `<div class="gallery__item"><a class="gallery__link" href="${original}"><img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/></a></div>`).join("");
 gallery.insertAdjacentHTML('beforeend', markup); 
 
 
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,39 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery2 = document.querySelector(".gallery");
+const markup2 = galleryItems.map(({ preview, original, description }) => `<li><a class="gallery__item" href="${original}"><img class="gallery__image" src="${preview}" alt="${description}"/></a></li>`).join("");
+gallery2.insertAdjacentHTML('beforeend', markup2);
+
+console.log(SimpleLightbox)
+// const lightbox = $(".gallery").simpleLightbox(); 
+// lightbox.on('show.simplelightbox', () => {
+//     const link = document.querySelector(".gallery__item");
+//     const image = document.createElement("img");
+//     image.src = `${link.getAttribute("href")}`;
+// });
+    
+gallery2.addEventListener("click", (evt) => {
+    evt.preventDefault();
+    if (evt.target.nodeName !== "IMG") {
+        return 
+    }
+});
+
+
+
+// function onClick2(evt) {
+//     evt.preventDefault();
+//     if (evt.target.nodeName !== "IMG") {
+//         return 
+//     }
+//     console.log(evt)
+
+//     const lightbox = new SimpleLightbox(".gallery"); 
+//     lightbox.on('show.simplelightbox', () => {
+//         const link = document.querySelector(".gallery__item");
+//         `<img src="${link.getAttribute("href")}" alt="" width="800", height="600"></a>`
+//     });
+// }
+
+// gallery2.addEventListener("click", onClick2)
