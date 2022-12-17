2ab82bf0b1fe1ef8e5092c7b157bf64692f50c24
Nastasiia03 <abernatskao@gmail.com>
2022-12-17 21:05:05 +0200

    updated

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -6,34 +6,11 @@ const markup2 = galleryItems.map(({ preview, original, description }) => `<li><a
 gallery2.insertAdjacentHTML('beforeend', markup2);
 
 console.log(SimpleLightbox)
-// const lightbox = $(".gallery").simpleLightbox(); 
-// lightbox.on('show.simplelightbox', () => {
-//     const link = document.querySelector(".gallery__item");
-//     const image = document.createElement("img");
-//     image.src = `${link.getAttribute("href")}`;
-// });
-    
-gallery2.addEventListener("click", (evt) => {
-    evt.preventDefault();
-    if (evt.target.nodeName !== "IMG") {
-        return 
-    }
-});
 
+   const lightbox = new SimpleLightbox(".gallery a", {captionsData: "alt", captionDelay: 250}); 
+    lightbox.on('show.simplelightbox', () => {
+        const link = document.querySelectorAll(".gallery__item");
+        link.map((evt)=> `<img src="${link.getAttribute("href")}" alt="" width="800", height="600"/>`)
+        
+    });
 
-
-// function onClick2(evt) {
-//     evt.preventDefault();
-//     if (evt.target.nodeName !== "IMG") {
-//         return 
-//     }
-//     console.log(evt)
-
-//     const lightbox = new SimpleLightbox(".gallery"); 
-//     lightbox.on('show.simplelightbox', () => {
-//         const link = document.querySelector(".gallery__item");
-//         `<img src="${link.getAttribute("href")}" alt="" width="800", height="600"></a>`
-//     });
-// }
-
-// gallery2.addEventListener("click", onClick2)
